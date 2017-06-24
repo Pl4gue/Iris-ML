6b9052ff5b946d4b3b566485757085344808e98d
David Wu <david10608@gmail.com>
2017-06-24 16:52:38 +0200

    Update Main
    Completed Main for first prediction tests on sklearn.database -> iris_data

--- Main.py
+++ Main.py
@@ -1,13 +1,20 @@
-import pandas
-from pandas.tools.plotting import scatter_matrix
-import matplotlib.pyplot as plt
-from sklearn import model_selection
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+import pandas as pd
+import numpy as np
+
+iris_dataset=load_iris()
+
+X_train,X_test,y_train,y_test=train_test_split(iris_dataset['data'],iris_dataset['target'],random_state=0)
+
+knn= KNeighborsClassifier(n_neighbors=1)
+print(knn.fit(X_train,y_train))
+
+X_new= np.array([[5,2.9,1,0.2]])
+
+y_predict=knn.predict(X_test)
+print(y_predict) #predicted iris categories for test values in X_test
+
+print("accuracy: {:.2f}".format(np.mean(y_predict==y_test))+
+	" | {:.2f}".format(knn.score(X_test,y_test)))
